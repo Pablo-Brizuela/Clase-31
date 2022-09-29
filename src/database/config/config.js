b86fab9bbf80149bddc15e7fba149e03a627d622
Pablo-Brizuela <pablo_2002_cab_@hotmail.com>
2022-09-29 15:10:06 -0300

    Configurando la clave de la base

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "database_development",
+    "password": "Pablo@123",
+    "database": "movies_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "Pablo@123",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": "Pablo@123",
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
